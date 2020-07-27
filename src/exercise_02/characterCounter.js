fbcc208ab06588ddc53c2e2a2b324afb23b6dcab
Rng-nb <18782824515@163.com>
2020-07-27 17:01:12 +0800

    [yafeng Wang] fix: fix the charactercounter function

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -5,6 +5,19 @@ export default function countCharacter(string, prediction) {
   //
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  let answer;
+  if (string === null || string === undefined || string.length === 0) {
+    answer = 0;
+  } else if (prediction === undefined) {
+    answer = string.length;
+  } else {
+    let num = 0;
+    for (let i = 0; i < string.length; i += 1) {
+      if (prediction(string[i]) === true) {
+        num += 1;
+      }
+    }
+    answer = num;
+  }
+  return answer;
 }

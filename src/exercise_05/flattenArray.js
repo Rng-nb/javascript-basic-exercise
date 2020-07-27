94c9b8939c8100c182a12d3c2193e46b60b8d245
Rng-nb <18782824515@163.com>
2020-07-27 18:05:27 +0800

    [yafeng Wang] fix: fix the flattenArray function

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -5,6 +5,20 @@ export default function flattenArray(array) {
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (array === undefined || array === null) {
+    throw new Error('Flatten undefined or null array');
+  } else {
+    // eslint-disable-next-line no-array-constructor
+    const answer = new Array();
+    for (let i = 0; i < array.length; i += 1) {
+      if (typeof (array[i]) === 'number') {
+        answer.push(array[i]);
+      } else {
+        for (let j = 0; j < array[i].length; j += 1) {
+          answer.push(array[i][j]);
+        }
+      }
+    }
+    return answer;
+  }
 }

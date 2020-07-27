c9f58188fc324116bcfcf6e70f3020843763eb41
Rng-nb <18782824515@163.com>
2020-07-27 17:45:34 +0800

    [yafeng Wang] fix: fix the arraytolist function

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,22 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  function MyList(value) {
+    this.value = value;
+    this.next = null;
+  }
+  if (array === null || array === undefined) {
+    throw new Error('Creating list from undefined array');
+  } else if (array.length === 0) {
+    throw new Error('Creating list from empty array');
+  } else {
+    let ListTmp = new MyList(array[0]);
+    const ListAnswer = ListTmp;
+    for (let i = 1; i < array.length; i += 1) {
+      const ListNext = new MyList(array[i]);
+      ListTmp.next = ListNext;
+      ListTmp = ListNext;
+    }
+    return ListAnswer;
+  } 
 }

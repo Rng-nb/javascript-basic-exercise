83a51fad85bc6fc2b0fa6eec85a3b09f64fc1e76
Rng-nb <18782824515@163.com>
2020-07-27 21:08:01 +0800

    [yafeng Wang] fix: fix the waitForAll fuction

--- src/exercise_07/waitForAll.js
+++ src/exercise_07/waitForAll.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-loop-func */
 export default function waitForAll(...promises) {
   // This function returns a promise which will be triggered when all the `promises` are completed.
   //
@@ -8,6 +9,32 @@ export default function waitForAll(...promises) {
   //
   // * Please implement this function and pass all the tests in wait_for_all_spec.js.
   // * Please do NOT modify the signature of the function.
+  for (let i = 0; i < promises.length; i += 1) {
+    if (typeof (promises[i]) !== 'object') {
+      throw new Error('Not all elements are promises.');
+    }
+  }
 
-  throw new Error('Please delete this line and implement the function');
+  return new Promise((resolve, reject) => {
+    let rejected = false;
+    let successNumber = 0;
+    for (let i = 0; i < promises.length; i += 1) {
+      (promises[i]).then(() => {
+        successNumber += 1;
+        if (successNumber === promises.length) {
+          if (rejected) {
+            reject();
+          } else {
+            resolve();
+          }
+        }
+      }, () => {
+        rejected = true;
+        successNumber += 1;
+        if (successNumber === promises.length) {
+          reject();
+        }
+      });
+    }
+  });
 }
